cb55c0c8f14362660d3a160726387ac7769b472f
Diego-2810 <diegoemilianoalganaras@gmail.com>
2025-10-24 16:16:02 -0300

    Cambios raros

--- WebBase/obj/Debug/net9.0/WebBase.AssemblyInfo.cs
+++ WebBase/obj/Debug/net9.0/WebBase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66de5bea61d3c9a1bdb37b5677b34b715e270a5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dd2db97e93d6a21c655544ab4fd0bcae7b9a1ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
